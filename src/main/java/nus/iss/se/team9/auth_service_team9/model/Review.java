d6fd1eca33bcd68dd9b0df275457aa3a1a840999
ColdSharpTen <E1221820@n.nus.edu>
2024-10-10 00:40:41 +0800

    Update Jackson in Model

--- src/main/java/nus/iss/se/team9/auth_service_team9/model/Review.java
+++ src/main/java/nus/iss/se/team9/auth_service_team9/model/Review.java
@@ -1,5 +1,6 @@
 package nus.iss.se.team9.auth_service_team9.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
@@ -16,21 +17,23 @@ public class Review {
 	@Column
 	private LocalDate reviewDate;
 	@ManyToOne
+	@JsonBackReference
 	private Member member;
 	@ManyToOne
+	@JsonBackReference
 	private Recipe recipe;
-	
+
 	public Review() {
 		setReviewDate(LocalDate.now());
 	}
-	
+
 	public Review(int rating, String comment, Member member, Recipe recipe) {
 		this.rating = rating;
 		this.comment = comment;
 		this.member = member;
 		this.recipe = recipe;
 	}
-	
+
 	// getter and setter
 	public Integer getId() {
 		return id;
